c7d9761fc0b8b18c8ee09f1acbd837465fa669ae
lorenlang <loren@lorenlang.com>
2015-08-10 21:04:14 -0400

    Logout and edit links if logged in

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,6 +23,13 @@
                 </p>
             @endunless
 
+            @if(Auth::check())
+                <ul class="list-inline pull-right">
+                    <li><a href="{{ url( '/posts/' . $post->slug . '/edit' ) }}"><i class="icon-edit icon-2x"></i></a>
+                    </li>
+                </ul>
+            @endif
+
                 <p class="publish-date">{{ $post->published_at->format('F d, Y') }}</p>
                 {{--<p class="publish-date">{{ $post->published_at }}</p>--}}
 
