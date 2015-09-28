e079314b669aaf3a860639035ef16eeab44076b0
lorenlang <loren@lorenlang.com>
2015-09-28 15:02:24 -0400

    Add Instagram link to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -16,6 +16,7 @@
                     <li><a href="https://twitter.com/lorenlang"><i class="icon-twitter icon-2x"></i></a></li>
                     {{--<li><i class="icon-google-plus icon-2x"></i></li>--}}
                     {{--<li><i class="icon-pinterest icon-2x"></i></li>--}}
+                    <li><a href="https://instagram.com/lorenlang00"><i class="icon-instagram icon-2x"></i></a></li>
                     <li><a href="https://www.linkedin.com/in/lorenlang"><i class="icon-linkedin icon-2x"></i></a></li>
                     <li><a href="http://wheresmyhead.com/feed"><i class="icon-rss icon-2x"></i></a></li>
                 </ul>
